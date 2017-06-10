3dba09d9c98c21052a19487b2852ff9e67d5b3ec
thipokKub <thipok30386@gmail.com>
2017-06-11 00:12:09 +0700

    Add date picker

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -41,49 +41,47 @@ class homePage extends Component {
 
         return (
             <section role="main-content" onClick={this.onClickMe}>
-                <DatePicker />
+                <h1 className="display-none">Main body</h1>
+                <section role="carousel">
+                    <h2 className="display-none">Carousel</h2>
+                    <SlickCarousel onItemPopUpClick={this.onItemPopUpClick} />
+                </section>
+                <div className="below-carousel">
+                    <section content="upcomming">
+                        <h2>Upcomming</h2>
+                        <CardList onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                    </section>
+                    <section content="bottom-half">
+                        <section content="tag">
+                            <h2>Tag</h2>
+                            <CircleList parent="tag" />
+                        </section>
+                        <section content="new">
+                            <h2>New</h2>
+                            <EventItem posterSrc={posterTest[0]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                            <EventItem posterSrc={posterTest[1]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                            <EventItem posterSrc={posterTest[2]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                            <EventItem posterSrc={posterTest[3]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                        </section>
+                    </section>
+                    <section content="bottom-half">
+                        <section content="channel">
+                            <h2>Channel</h2>
+                            <ChannelList />
+                        </section>
+                        <section content="for-you">
+                            <h2>For you</h2>
+                            <EventItem posterSrc={posterTest[4]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                            <EventItem posterSrc={posterTest[5]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                            <EventItem posterSrc={posterTest[6]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                            <EventItem posterSrc={posterTest[7]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
+                        </section>
+                    </section>
+                </div>
             </section>
         );
     }
 }
-
-// <h1 className="display-none">Main body</h1>
-// <section role="carousel">
-//     <h2 className="display-none">Carousel</h2>
-//     <SlickCarousel onItemPopUpClick={this.onItemPopUpClick} />
-// </section>
-// <div className="below-carousel">
-//     <section content="upcomming">
-//         <h2>Upcomming</h2>
-//         <CardList onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//     </section>
-//     <section content="bottom-half">
-//         <section content="tag">
-//             <h2>Tag</h2>
-//             <CircleList parent="tag" />
-//         </section>
-//         <section content="new">
-//             <h2>New</h2>
-//             <EventItem posterSrc={posterTest[0]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//             <EventItem posterSrc={posterTest[1]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//             <EventItem posterSrc={posterTest[2]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//             <EventItem posterSrc={posterTest[3]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//         </section>
-//     </section>
-//     <section content="bottom-half">
-//         <section content="channel">
-//             <h2>Channel</h2>
-//             <ChannelList />
-//         </section>
-//         <section content="for-you">
-//             <h2>For you</h2>
-//             <EventItem posterSrc={posterTest[4]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//             <EventItem posterSrc={posterTest[5]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//             <EventItem posterSrc={posterTest[6]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//             <EventItem posterSrc={posterTest[7]} detail-shown="true" onToggle={this.props.toggle_pop_item} onSetItem={this.props.set_pop_up_item} />
-//         </section>
-//     </section>
-// </div>
 
 
 export default normalPage(pages(homePage, true));
