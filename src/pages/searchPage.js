62538c4651fffb43c592e26f02c511d158dfef61
focusjk <jiraphat.khupanit@gmail.com>
2017-06-16 01:03:23 +0700

    no message

--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -1,31 +1,47 @@
 import React, { Component } from 'react';
 import '../container/css/eventDetail2.css'
+import './css/searchPage.css'
 import EventItem from '../container/eventItem';
 
 let isSet = false;
 //https://codepen.io/bh/pen/JBlCc
 
-class eventDetailFix extends Component {
+class searchPage extends Component {
 
     onExit() {
         this.props.onToggle();
     }
 
     render() {
-
-        let eventName = (
-            <div className="event-name">
-                <h2>Event Name</h2>
-                <div><img alt="channel-icon" /> Channel Name</div>
-            </div>
-        );
-
         return (
+
             <div className="modal-container">
-             <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="true" />
+                <article className="event-search">
+                    <div className="keyword">Keyword</div>
+                    <div className="found">found 17 results</div>
+                    <div className="event-found">
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="true" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="true" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="true" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="true" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+                        <EventItem posterSrc={`../../resource/images/poster_dummy/1.jpg`} detail-shown="false" />
+
+
+                    </div>
+                </article>
+                <div className="background-overlay-fix"/>
             </div>
+
         );
     }
 }
 
-export default eventDetailFix;
+export default searchPage;
